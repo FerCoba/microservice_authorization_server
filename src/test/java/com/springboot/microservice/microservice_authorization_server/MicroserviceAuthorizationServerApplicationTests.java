b87df15d4df4c9776cb160f0ca956e4b49a5d782
fernandocoba <fernandocoba@gmail.com>
2020-04-11 19:53:21 -0300

    first commit

---
+++ src/test/java/com/springboot/microservice/microservice_authorization_server/MicroserviceAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.microservice.microservice_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
